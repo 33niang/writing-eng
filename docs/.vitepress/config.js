19eeb89099bb544bbe2c1bdc2164a492173e72bf
33niang <76659394+33niang@users.noreply.github.com>
2025-10-19 21:43:47 +0800

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,17 +4,25 @@ export default {
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Essays', link: '/essay/18' }
+      { text: 'Essays', link: '/essay/2018/major' } // 导航栏直接链接到第一篇文章
     ],
     sidebar: {
       '/essay/': [
         {
-          text: 'Essays',
+          text: '2018', // 按年份分组
           items: [
-            { text: '2018-大作文', link: '/essay/18' },
-            // 后续你可以在这里添加更多作文
+            { text: '大作文', link: '/essay/2018/major' },
+            // 后期你可以在这里添加小作文, 例如:
+            // { text: '小作文', link: '/essay/2018/minor' }
           ]
         }
+        // 后续可以继续添加新的年份分组
+        // {
+        //   text: '2019',
+        //   items: [
+        //     { text: '大作文', link: '/essay/2019/major' }
+        //   ]
+        // }
       ]
     }
   }
